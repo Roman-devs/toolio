e1a10554eaa550a2ae3ac8111711071354f04335
Roman Kuite <roman.kuite@gmail.com>
2021-03-10 16:58:34 +0100

    Init backend

---
+++ backend/src/main/java/de/roman/toolio/ToolioApplication.java
@@ -0,0 +1,13 @@
+package de.roman.toolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolioApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/roman/toolio/ToolioApplicationTests.java
@@ -0,0 +1,13 @@
+package de.roman.toolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
