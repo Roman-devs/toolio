9c4fa1bf3ce7bbcbd4190be56c9d73c5750fc322
Roman Kuite <roman.kuite@gmail.com>
2021-03-15 15:17:42 +0100

    Added MongoDB connection, Initialized Model, Service and Database

---
+++ backend/src/main/java/de/roman/toolio/model/InquiryPart.java
@@ -0,0 +1,24 @@
+package de.roman.toolio.model;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection="inquiryParts")
+public class InquiryPart {
+    // Dimensions
+    private String lengthPart;
+    private String widthPart;
+    private String heightPart;
+    // Material used for production
+    private String materialPart;
+    // Amount of Parts that are desired
+    private String amountPart;
+    // Pictures to be uploaded
+    // Intentionally left blank - to be implemented
+}
