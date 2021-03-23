2fc8e538f5852644052ad7a9e8edbb17129f88e0
Roman Kuite <roman.kuite@gmail.com>
2021-03-23 15:14:28 +0100

    WIP - Adapted styling of the card to the form

--- frontend/src/components/CardStyling.js
+++ frontend/src/components/CardStyling.js
@@ -8,6 +8,7 @@ export const CardWrapper = styled.div`
   font-family: "Courier New", arial, sans-serif;
   border-radius: 15px;
   box-shadow: 0 0 20px darkgrey;
+  text-align: center;
 `;
 
 export const CardImage = styled.div`
@@ -52,7 +53,7 @@ export const ProductDimensionsWrapper = styled.div`
   display:flex;
   flex-direction: row;
   justify-content: space-evenly;
-  padding: 12.5px 25px 25px 25px;
+  padding: 12.5px 25px 5px 25px;
   box-sizing: border-box;
   // flex space between;
   font-size: 1em;
@@ -75,4 +76,25 @@ export const ProductDimension = styled.div`
   flex-direction: column;
   padding: 0;
   justify-content: center;
+`
+
+export const CardButton = styled.button`
+  display: inline-block;
+  padding: 12px;
+  margin: 5px;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: 700;
+  color: #ffffff;
+  background-color: #000000;
+  border: 2px;
+  border-radius: 25px;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.08);
+  cursor: pointer;
+  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.4, 1);
+
+  &:hover {
+    box-shadow: 0 15px 15px rgba(0, 0, 0, 0.1);
+    transform: translate(0, -1px);
+  }
 `
