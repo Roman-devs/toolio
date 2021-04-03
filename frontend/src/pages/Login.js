5d3573826f2940f6948c989c7ad7f6be3b8cfa94
Roman Kuite <roman.kuite@gmail.com>
2021-04-03 17:37:17 +0200

    FRONTEND: Added Styling to Login Page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,31 @@
+import {useForm} from "react-hook-form";
+import {Styles} from "../styling/FormStyling";
+import { useState } from 'react'
 
 export default function Login (){
-    return(
-        <>
-        <p>Login Page</p>
-        </>
+    const {register, handleSubmit, errors} = useForm();
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    const onSubmit = userData => {
+        console.log(userData)
+    }
+
+    return (
+        <Styles>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <h1>TooLio</h1>
+                <label>Username</label>
+                <input name="username"
+                       ref={register({required: true})}/>
+                {errors.username && "Required"}
+
+                <label>Password</label>
+                <input name="password"
+                       ref={register({required: true})}/>
+                {errors.password && "Required"}
+
+                <input className="submitButton" type="submit" value="Login!"/>
+            </form>
+        </Styles>
     )
 }
