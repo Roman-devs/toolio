78382ceb66ca806fe9709f5f82a28f08008812d9
Roman Kuite <roman.kuite@gmail.com>
2021-04-14 14:00:41 +0200

    Added react router

--- backend/src/main/java/de/roman/toolio/ToolioApplication.java
+++ backend/src/main/java/de/roman/toolio/ToolioApplication.java
@@ -3,10 +3,11 @@ package de.roman.toolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties
-public class ToolioApplication {
+public class ToolioApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ToolioApplication.class, args);
     }
