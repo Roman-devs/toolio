86a877ca81d57e479276b3572b6c2099c4dcf8a1
Roman Kuite <roman.kuite@gmail.com>
2021-04-14 14:25:57 +0200

    Add Rest Template Config

--- backend/src/main/java/de/roman/toolio/ToolioApplication.java
+++ backend/src/main/java/de/roman/toolio/ToolioApplication.java
@@ -3,11 +3,10 @@ package de.roman.toolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties
-public class ToolioApplication extends SpringBootServletInitializer {
+public class ToolioApplication {
     public static void main(String[] args) {
         SpringApplication.run(ToolioApplication.class, args);
     }
